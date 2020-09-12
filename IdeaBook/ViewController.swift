93587f3f8aea2dfd8ff52069ee6c0f7ca0760fc1
MaksPlank <6677limb@gmail.com>
2020-09-12 16:22:55 +0300

    Initial Commit

---
+++ IdeaBook/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IdeaBook
+//
+//  Created by Maks Plank on 12.09.2020.
+//  Copyright © 2020 Natalia Golovko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
