7b0c4aecfbf85c841ae5d4e2f7c2d0f49b011fb6
MaksPlank <6677limb@gmail.com>
2020-09-13 19:13:59 +0300

    Method 2

--- TableView.swift
+++ TableView.swift
@@ -10,21 +10,46 @@ import UIKit
 
 class TableView: UITableView {
     
-    // Custom TableView
-    // TableView is Subclass UITableView
-
-    @IBOutlet weak var custHeight: NSLayoutConstraint!
+    
+    var custHeight: NSLayoutConstraint?
+    var custBottom: NSLayoutConstraint?
+    
     
     override func layoutSubviews() {
-        super .layoutSubviews()
+    super .layoutSubviews()
         
         guard let header = tableHeaderView else { return }
+        if let image = header.subviews.first as? UIImageView {
+            custHeight = image.constraints.filter{ $0.identifier == "heightInit" }.first
+            custBottom = header.constraints.filter{ $0.identifier == "bottomInit" }.first
+            }
+        
         let offSetY = -contentOffset.y
-        // print(offSetY) // см в консчоли изменение величины
+        custHeight?.constant = max(header.bounds.height, header.bounds.height + offSetY)
+        custBottom?.constant = offSetY >= 0 ? 0 : offSetY / 2
         
-        // change height (custHeight)
-        custHeight.constant = max(header.bounds.height, header.bounds.height + offSetY)
-        
+        header.clipsToBounds = offSetY <= 0
         }
-    
 }
+    
+    
+    
+    
+//    // Custom TableView
+//    // TableView is Subclass UITableView
+//
+//    @IBOutlet weak var custHeight: NSLayoutConstraint!
+//
+//    override func layoutSubviews() {
+//        super .layoutSubviews()
+//
+//        guard let header = tableHeaderView else { return }
+//        let offSetY = -contentOffset.y
+//        // print(offSetY) // см в консчоли изменение величины
+//
+//        // change height (custHeight)
+//        custHeight.constant = max(header.bounds.height, header.bounds.height + offSetY)
+//
+//        }
+//
+
